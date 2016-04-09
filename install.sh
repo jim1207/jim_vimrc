864d710e76f66aa9da53df8f87a5098ff48c73ef
root <root@localhost.localdomain>
2016-04-10 02:01:13 +0800

    up

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ln -ns jim_vimrc/dot_vimrc ~/.vimrc
 ln -ns jim_vimrc/dot_vim ~/.vim
 ln -ns jim_vimrc/dot_byobu ~/.byobu
-ln -ns jim_vimrc/.tmuxifier ~/.tuxifier
+ln -ns jim_vimrc/dot_tmuxifier ~/.tmuxifier
 
