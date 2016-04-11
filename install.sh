1da0b9286a8f85ac91024fc93ed11b2353ebe8f8
root <root@localhost.localdomain>
2016-04-11 15:00:47 +0800

    up

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
-ln -ns jim_vimrc/dot_vimrc ~/.vimrc
-ln -ns jim_vimrc/dot_vim ~/.vim
-ln -ns jim_vimrc/dot_byobu ~/.byobu
-ln -ns jim_vimrc/dot_tmux.conf ~/.tmux.conf
-ln -ns jim_vimrc/dot_tmuxifier ~/.tmuxifier
-ln -ns jim_vimrc/dot_bashrc ~/.bashrc
+[ ! -L ~/.vimrc ]      && ln -ns jim_vimrc/dot_vimrc ~/.vimrc
+[ ! -L ~/.vim ]        && ln -ns jim_vimrc/dot_vim ~/.vim
+[ ! -L ~/.byobu ]      && ln -ns jim_vimrc/dot_byobu ~/.byobu
+[ ! -L ~/.tmux.conf ]  && ln -ns jim_vimrc/dot_tmux.conf ~/.tmux.conf
+[ ! -L ~/.tmuxifier ]  && ln -ns jim_vimrc/dot_tmuxifier ~/.tmuxifier
+
+#append bashrc.jim to ~/.bashrc
+str=bashrc.jim
+exe=`grep $str $HOME/.bashrc`
+if [ x"$exe" == x ]; then
+    echo "source ~/jim_vimrc/$str" >> $HOME/.bashrc
+fi
 
