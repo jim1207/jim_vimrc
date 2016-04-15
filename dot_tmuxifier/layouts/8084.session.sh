395ab306f4ffc51846c9329a644cb8f22dfef448
Jim <jim.jm.chen@foxconn.com>
2016-04-16 02:10:47 +0800

    up

--- dot_tmuxifier/layouts/8084.session.sh
+++ dot_tmuxifier/layouts/8084.session.sh
@@ -1,10 +1,10 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-#session_root "~/Projects/s2"
+#session_root "~/Projects/8084"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "s2"; then
+if initialize_session "8084"; then
 
   # Create a new window inline within session layout definition.
   new_window "free-use"
